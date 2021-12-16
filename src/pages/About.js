5c6f4656bfec2b8abd4732d1b8a4d0fa7d2853fe
EmilHarutyunyan <emil.h098@gmail.com>
2021-12-16 22:00:50 +0400

    cocktails

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi distinctio id, nihil excepturi earum sapiente esse fugiat saepe, possimus quasi, culpa eos fugit non vero voluptates quaerat. Quibusdam ad quia facilis recusandae. Odio fuga tenetur magnam praesentium facilis consectetur repellendus?
+      </p>
+    </section>
+  )
+}
+
+export default About
